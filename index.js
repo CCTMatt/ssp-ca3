e69a578621acea67be5098b3991f0d1f6225b12b
CCTMatt <2021390@student.cct.ie>
2023-01-02 22:51:50 +0000

    website, custom icon.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const  http = require('http'), //HTTP Server
        path = require('path'),
-       express = require('express'), //Handling HTTP requests and routing
+       expr = require('express'), //Handling HTTP requests and routing
        fs = require('fs'),  //File System functionalities
        xmlParse = require('xslt-processor').xmlParse, //XML handling
        xsltProcess = require('xslt-processor').xsltProcess, //XSLT handling
-       router = express(), //Init our router
-       server = http.createServer(router); // Init our server
+       router = expr(), //Init the router
+       serv = http.createServer(router); // Init the server
 
-router.use(express.static(path.resolve(__dirname, 'views')));
-router.use(express.urlencoded({extended: true}));
-router.use(express.json());
+router.use(expr.static(path.resolve(__dirname, 'views')));
+router.use(expr.urlencoded({extended: true}));
+router.use(expr.json());
 
-router.get('/', function(req, res) {
+router.get('/get/html', function(req, res) {
 
     res.writeHead(200, {'Content-Type' : 'text/html'});
 
@@ -26,8 +26,8 @@ router.get('/', function(req, res) {
     res.end(html.toString());
 });
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function()
+serv.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function()
 {
-    const addr = server.address();
+    const addr = serv.address();
     console.log("Server listening at", addr.address + ":" + addr.port)
 });
